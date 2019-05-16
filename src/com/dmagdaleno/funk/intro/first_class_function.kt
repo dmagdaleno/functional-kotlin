ee6b93641c022a75351910e783e94a1fe8727e2a
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-15 22:37:45 -0300

    object method use with double colon

--- src/com/dmagdaleno/funk/intro/first_class_function.kt
+++ src/com/dmagdaleno/funk/intro/first_class_function.kt
@@ -8,9 +8,17 @@ fun transform(str:String, func: (String) -> String): String {
     return func(str)
 }
 
+object Obj {
+    fun reverse(str: String): String {
+        return str.reversed()
+    }
+}
+
 fun main(args: Array<String>) {
 
     println(capitalize("hello world!"))
 
     println(transform("test", capitalize))
+
+    println(transform("test", Obj::reverse))
 }
