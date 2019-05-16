56d56ebe9679a4aa8861e662a2860ebd6c581564
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-15 22:28:32 -0300

    higher-order function

--- src/com/dmagdaleno/funk/intro/first_class_function.kt
+++ src/com/dmagdaleno/funk/intro/first_class_function.kt
@@ -1,6 +1,16 @@
 package com.dmagdaleno.funk.intro
 
+// lambda function
 val capitalize = { str: String -> str.capitalize() }
+
+// higher-order function
+fun transform(str:String, func: (String) -> String): String {
+    return func(str)
+}
+
 fun main(args: Array<String>) {
+
     println(capitalize("hello world!"))
+
+    println(transform("test", capitalize))
 }
