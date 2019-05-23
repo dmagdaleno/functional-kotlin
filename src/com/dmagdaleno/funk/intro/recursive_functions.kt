be231157421173bc0d48722821f4c745169c63d1
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-22 22:27:07 -0300

    non-recursive (imperative) implementation

--- src/com/dmagdaleno/funk/intro/recursive_functions.kt
+++ src/com/dmagdaleno/funk/intro/recursive_functions.kt
@@ -1,2 +1,14 @@
 package com.dmagdaleno.funk.intro
 
+// non-recursive (imperative) way
+fun factorial(n: Long): Long {
+    var result = 1L
+    for (i in 1..n) {
+        result *= i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    println(factorial(n))
+}
