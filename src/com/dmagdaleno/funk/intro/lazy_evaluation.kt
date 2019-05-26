38362578c5ed6ecc33702cacb85ca8584bfe9608
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-25 23:33:12 -0300

    example of lazy evaluation

--- src/com/dmagdaleno/funk/intro/lazy_evaluation.kt
+++ src/com/dmagdaleno/funk/intro/lazy_evaluation.kt
@@ -10,4 +10,11 @@ fun main(args: Array<String>) {
     println("before using i")
 
     println(i)
+
+    // throw exception 'ArithmeticException: / by zero'
+    //val size = listOf(2 + 1, 3 * 2, 1 / 0, 5 - 4).size
+
+    // this version does not throw exception until the value is used
+    val size = listOf({ 2 + 1 }, { 3 * 2 }, { 1 / 0 }, { 5 - 4 }).size
+    println(size)
 }
