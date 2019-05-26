5bc4cec1fcd8104992e2e5fc65c92315cdc49d65
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-25 23:26:41 -0300

    add a lazy init example

--- src/com/dmagdaleno/funk/intro/lazy_evaluation.kt
+++ src/com/dmagdaleno/funk/intro/lazy_evaluation.kt
@@ -1,2 +1,13 @@
 package com.dmagdaleno.funk.intro
 
+fun main(args: Array<String>) {
+
+    val i by lazy {
+        println("Lazy evaluation")
+        10
+    }
+
+    println("before using i")
+
+    println(i)
+}
