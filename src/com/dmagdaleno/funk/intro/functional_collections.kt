821b40b6a7126db168e03ead012c7fbabd032625
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-26 23:11:25 -0300

    map a list to another

--- src/com/dmagdaleno/funk/intro/functional_collections.kt
+++ src/com/dmagdaleno/funk/intro/functional_collections.kt
@@ -1,2 +1,18 @@
 package com.dmagdaleno.funk.intro
 
+val numbers: List<Int> = listOf(1, 2, 3, 4)
+
+fun main(args: Array<String>) {
+
+    // imperative way
+    val imperative: MutableList<Int> = mutableListOf()
+    for (i in numbers) {
+        imperative.add(i * 2)
+    }
+
+    // functional way
+    val functional = numbers.map { i -> i * 2 }
+
+    println("Imperative: ${imperative.joinToString()}")
+    println("Functional: ${functional.joinToString()}")
+}
