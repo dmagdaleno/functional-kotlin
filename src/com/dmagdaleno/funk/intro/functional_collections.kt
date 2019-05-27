d05f2b6eae8054426038265691dd9ad09f2c6da2
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-26 23:18:22 -0300

    sum items of a list

--- src/com/dmagdaleno/funk/intro/functional_collections.kt
+++ src/com/dmagdaleno/funk/intro/functional_collections.kt
@@ -1,22 +1,39 @@
 package com.dmagdaleno.funk.intro
 
+val numbers: List<Int> = listOf(1, 2, 3, 4)
+
 fun main(args: Array<String>) {
 
     mapAListToAnother()
+
+    sumItemsOfAList()
 }
 
 private fun mapAListToAnother() {
-    val numbers: List<Int> = listOf(1, 2, 3, 4)
+    println()
 
     // imperative way
     val imperative: MutableList<Int> = mutableListOf()
     for (i in numbers) {
         imperative.add(i * 2)
     }
+    println("Imperative: ${imperative.joinToString()}")
 
     // functional way
     val functional = numbers.map { i -> i * 2 }
-
-    println("Imperative: ${imperative.joinToString()}")
     println("Functional: ${functional.joinToString()}")
+}
+
+private fun sumItemsOfAList() {
+    println()
+
+    // imperative way
+    var sum = 0
+    for (i in numbers) {
+        sum += i
+    }
+    println("Imperative: $sum")
+
+    // functional way
+    println("Functional: ${numbers.sum()}")
 }
