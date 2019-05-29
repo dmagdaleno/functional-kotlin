459138a92be958d27db33abd7346b05082d3fe64
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-28 21:39:38 -0300

    add a better functional list initialization

--- src/com/dmagdaleno/funk/intro/functional_list.kt
+++ src/com/dmagdaleno/funk/intro/functional_list.kt
@@ -1,9 +1,20 @@
 package com.dmagdaleno.funk.intro
 
+import com.dmagdaleno.funk.intro.classes.MyList
 import com.dmagdaleno.funk.intro.classes.MyList.Cons
 import com.dmagdaleno.funk.intro.classes.MyList.Empty
 
-fun main(args: Array<String>) {
-    val numbers = Cons(1, Cons(2, Cons(3, Cons(4, Empty))))
-    println(numbers)
+fun main() {
+    
+    println(Cons(1, Cons(2, Cons(3, Cons(4, Empty)))))
+
+    println(intListOf(1, 2, 3, 4))
+}
+
+fun intListOf(vararg numbers: Int): MyList<Int> {
+    return if (numbers.isEmpty()) {
+        Empty
+    } else {
+        Cons(numbers.first(), intListOf(*numbers.drop(1).toTypedArray().toIntArray()))
+    }
 }
