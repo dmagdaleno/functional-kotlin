8c75d49608ebbb679e3e0d20d2aa631f0e75cb5d
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-05-28 22:54:05 -0300

    declare custom fold function

--- src/com/dmagdaleno/funk/intro/classes/MyList.kt
+++ src/com/dmagdaleno/funk/intro/classes/MyList.kt
@@ -19,4 +19,12 @@ sealed class MyList<out T> {
         go(this, f)
     }
 
+    fun <R> fold(init: R, f: (R, T) -> R): R {
+        tailrec fun go(list: MyList<T>, init: R, f: (R, T) -> R): R = when (list) {
+            is Cons -> go(list.tail, f(init, list.head), f)
+            is Empty -> init
+        }
+        return go(this, init, f)
+    }
+
 }
